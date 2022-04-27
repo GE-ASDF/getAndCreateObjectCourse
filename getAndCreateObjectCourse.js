95814814c8da4add73478cbb63d4b33dc77e6058
Anderson Souza <andersonsouza007@live.com>
2022-04-27 12:08:03 -0300

    Função para abrir todos os botões do site

--- getAndCreateObjectCourse.js
+++ getAndCreateObjectCourse.js
@@ -1,22 +1,39 @@
-function getCourseAndCreateObject(accordionMain){
-	let content = document.querySelectorAll(accordionMain)
-	let contentArray = Array.from(content);
-	this.createCourseObject = function(accordionSeconday){
-		let titulosCollection = contentArray.map(tituloCollection=>{
-		let titulos = tituloCollection.querySelector("h5");
-		let aulasNode = tituloCollection.querySelectorAll(".collapse > div div > .d-flex .pr-3 label a")
+function getCourseAndCreateObject(){
+	let accordionNode;
+	let buttonsArray;
+	let content;
+	let contentArray;
+	let titulosCollection;
+	let titulos;
+	let aulasNode;
+
+	this.openAllContent = function(accordionButtons){
+		accordionNode = document.querySelectorAll(accordionButtons)
+		buttonsArray = Array.from(accordionNode);
+		buttonsArray.forEach(i=>{i.click()})
+	}
+	
+	this.createCourseObject = function(accordionMain, tagTitle, accordionSecondary){
+		content = document.querySelectorAll(accordionMain)
+		contentArray = Array.from(content);
+		titulosCollection = contentArray.map(tituloCollection=>{
+		titulos = tituloCollection.querySelector(tagTitle);
+		aulasNode = tituloCollection.querySelectorAll(accordionSecondary)
 		let aulas = Array.from(aulasNode);
 		return{titulos, aulas}
 	})
+	
 		titulosCollection.forEach(titulo=>{
 			console.log(titulo.titulos.innerText)
 			titulo.aulas.forEach(aula=>{
 				console.log(aula.innerText);
 			})
 		})
+		
 	}
 }
 
-let content = new getCourseAndCreateObject("#accordion .card")
-content.createCourseObject(".collapse > div div > .d-flex .pr-3 label a")
+let content = new getCourseAndCreateObject()
+content.openAllContent("#accordion .card-header")
+content.createCourseObject("#accordion .card", "h5", ".collapse > div div > .d-flex .pr-3 label a")
 
